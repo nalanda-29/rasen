391e11f7613f5210d9da0a69a29e796cd0c10119
vinod_reddy <58871377+vinod901@users.noreply.github.com>
2023-02-18 18:08:23 +0000

    table program

---
+++ src/main/java/com/tylerhawkins/examples/Table.java
@@ -0,0 +1,9 @@
+package com.tylerhawkins.examples;
+public class Table {
+    public static void main(String[] args){
+        int num = 33;
+        for(int i=0;i<10;i++){
+            System.out.println(num+"*"+(i+1)+"="+num*(i+1));
+        }
+    }
+}
